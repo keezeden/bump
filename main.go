0ca04de9b1f91b80425669a8dc187a6eee4c39ad
Key-github <keezeden@gmail.com>
2020-06-08 23:20:58 +1200

    Added package.json read/write

--- main.go
+++ main.go
@@ -3,35 +3,91 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
 )
 
+const usage = `
+  Bump, the multi-repo package version bumping tool.
+
+	Usage:
+			
+	  @author/package version repo1 repo2 repo3
+
+	Example:
+				
+	  @keez/core_lib .27 cli_app web_app
+		`
+
+// Initializing overrides.
 func setup() {
-	// forgive me for i have sinned
 	flag.Usage = func() {
-		fmt.Println("\nBump, the multi-repo package version bumping tool.\n" +
-			"Usage:\n\t@author/package version repo1 repo2 repo3\n" +
-			"Example:\n\t@keez/core_lib .27 cli_app web_app \n")
-		flag.PrintDefaults()
+		fmt.Print(usage)
 	}
 }
 
 func main() {
-
 	setup()
 
-	recursive := flag.Bool("r", false, "Recursively bump all child repos in selected folder.")
+	var pkg, version, repos, recursive = parseArgs()
+
+	fmt.Println(recursive)
+
+	for _, repo := range repos {
+		var file = readPkg(repo)
+		var nFile, err = writePkg(file, repo, pkg, version)
+		if err != nil {
+			log.Fatalln("Error occured while updating %s: %s", repo, err)
+		}
+		updatePkg(repo, nFile)
+	}
+
+}
+
+func parseArgs() (string, string, []string, bool) {
+	recursive := flag.Bool("r", false, "")
 
 	flag.Parse()
-
 	args := flag.Args()
 
-	module := args[0]
+	pkg := args[0]
 	version := args[1]
 	repos := args[2:]
 
-	fmt.Println("Module to bump:", module)
-	fmt.Println("Version number:", version)
-	fmt.Println("Repos to update:", repos)
-	fmt.Println("Recursive?:", *recursive)
+	return pkg, version, repos, *recursive
+}
 
+func readPkg(repo string) []string {
+	file, err := ioutil.ReadFile(fmt.Sprintf("%s/package.json", repo))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	lines := strings.Split(string(file), "\n")
+
+	return lines
+}
+
+func writePkg(lines []string, repo string, pkg string, version string) (string, error) {
+	for i, line := range lines {
+		if strings.Contains(line, pkg) {
+			lines[i] = newVersion(line, version)
+		}
+	}
+
+	return strings.Join(lines, "\n"), nil
+}
+
+func updatePkg(repo string, nFile string) {
+	err := ioutil.WriteFile(fmt.Sprintf("%s/package_test.json", repo), []byte(nFile), 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func newVersion(line string, newVersion string) string {
+	split := strings.Split(line, ":")
+	split[1] = newVersion
+	return strings.Join(split, ":")
 }
