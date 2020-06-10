22e17f3768271107a0c8eea925def96cabbf47e5
Key-github <keezeden@gmail.com>
2020-06-10 23:17:08 +1200

    Added git branch/commit/push steps to version bump

--- main.go
+++ main.go
@@ -5,44 +5,113 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os/exec"
 	"strings"
 )
 
 const usage = `
-  Bump, the multi-repo package version bumping tool.
-
 	Usage:
-			
-	  @author/package version repo1 repo2 repo3
+	  bump <package> <version> [repos]
+	  bump <package> <version> -r <folder>
+	  bump -h
+	  
+	Options:
+	  -r	recursively bump subfolder repos
 
-	Example:
-				
+	Example:			
 	  @keez/core_lib .27 cli_app web_app
 		`
 
-// Initializing overrides.
-func setup() {
-	flag.Usage = func() {
-		fmt.Print(usage)
+type Branch struct {
+	Repo string
+}
+
+// Create new branch for version bump.
+func (b *Branch) New() {
+	// Check if branch already exists.
+	oldBranch := exec.Command("git", "checkout", "build/version-bump")
+	oldBranch.Dir = b.Repo
+	oErr := oldBranch.Run()
+	if oErr != nil {
+		b.Stash()
+
+		branch := exec.Command("git", "checkout", "-b", "build/version-bump")
+		branch.Dir = b.Repo
+		bErr := branch.Run()
+		if bErr != nil {
+			log.Fatalf("an error occured while creating branch: %v", bErr)
+		}
+	}
+}
+
+// Stash any unsaved changes.
+func (b *Branch) Stash() {
+	stash := exec.Command("git", "stash", "-u", "-k")
+	stash.Dir = b.Repo
+	sErr := stash.Run()
+	if sErr != nil {
+		log.Fatalf("an error occured while stashing changes: %v", sErr)
+	}
+}
+
+// Commit to branch.
+func (b *Branch) Commit() {
+	stage := exec.Command("git", "add", "--all", ".")
+	stage.Dir = b.Repo
+	commit := exec.Command("git", "commit", "-m", "build(bump): version bump")
+	commit.Dir = b.Repo
+	sErr := stage.Run()
+	if sErr != nil {
+		log.Fatalf("an error occured while staging changes: %v", sErr)
+	}
+	cErr := commit.Run()
+	if cErr != nil {
+		log.Fatalf("an error occured while comitting changes: %v", cErr)
+	}
+}
+
+// Push changes to origin.
+func (b *Branch) Push() {
+	push := exec.Command("git", "push", "--set-upstream", "origin", "build/version-bump")
+	push.Dir = b.Repo
+	err := push.Run()
+	if err != nil {
+		log.Fatalf("an error occured while pushing changes: %v", err)
 	}
 }
 
 func main() {
-	setup()
+	flag.Usage = func() {
+		fmt.Print(usage)
+	}
 
 	var pkg, version, repos, recursive = parseArgs()
 
-	fmt.Println(recursive)
-
-	for _, repo := range repos {
-		var file = readPkg(repo)
-		var nFile, err = writePkg(file, repo, pkg, version)
-		if err != nil {
-			log.Fatalln("Error occured while updating %s: %s", repo, err)
-		}
-		updatePkg(repo, nFile)
+	if recursive {
+		fmt.Println(recursive)
 	}
 
+	for _, repo := range repos {
+
+		path := fmt.Sprintf("./%s", repo)
+
+		branch := Branch{path}
+		branch.New()
+
+		file := readPkg(repo)
+
+		var nFile, err = writePkg(file, pkg, version)
+
+		if err != nil {
+			log.Fatalf("Error occured while updating %s: %v", repo, err)
+		}
+
+		updatePkg(repo, nFile)
+		fmt.Println("Updating Package...")
+
+		branch.Commit()
+		branch.Push()
+	}
 }
 
 func parseArgs() (string, string, []string, bool) {
@@ -58,10 +127,11 @@ func parseArgs() (string, string, []string, bool) {
 	return pkg, version, repos, *recursive
 }
 
+// Read repo package.json file into memory.
 func readPkg(repo string) []string {
 	file, err := ioutil.ReadFile(fmt.Sprintf("%s/package.json", repo))
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalf("%v", err)
 	}
 
 	lines := strings.Split(string(file), "\n")
@@ -69,7 +139,8 @@ func readPkg(repo string) []string {
 	return lines
 }
 
-func writePkg(lines []string, repo string, pkg string, version string) (string, error) {
+// Write repo package.json in memory with new version.
+func writePkg(lines []string, pkg string, version string) (string, error) {
 	for i, line := range lines {
 		if strings.Contains(line, pkg) {
 			lines[i] = newVersion(line, version)
@@ -79,15 +150,19 @@ func writePkg(lines []string, repo string, pkg string, version string) (string, 
 	return strings.Join(lines, "\n"), nil
 }
 
+// Write repo package.json in memory to file.
 func updatePkg(repo string, nFile string) {
-	err := ioutil.WriteFile(fmt.Sprintf("%s/package_test.json", repo), []byte(nFile), 0644)
+	err := ioutil.WriteFile(fmt.Sprintf("%s/package.json", repo), []byte(nFile), 0644)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalf("%v", err)
 	}
 }
 
+// Format new version.
 func newVersion(line string, newVersion string) string {
+	// TODO . seperator for easy version updates
 	split := strings.Split(line, ":")
-	split[1] = newVersion
+	split[1] = fmt.Sprintf("\"%s\",", newVersion)
+	fmt.Println("New Version", strings.Join(split, ":"))
 	return strings.Join(split, ":")
 }
